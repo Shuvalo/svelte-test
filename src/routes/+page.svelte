b9ec3936adc1f420920acb0fbfc307e08ee73fa9
Kristina <tinka200507@mail.ru>
2024-03-20 00:19:08 +0400

    add main page and nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,11 +29,11 @@
 <div class="background-div"></div>
 
 <style>
-    .info{
+    .info {
         text-align: center; 
         position: relative;
     }
-    .info-container{
+    .info-container {
         height: 100%; 
         display: flex;
         flex-direction: column;
@@ -49,7 +49,7 @@
     font-size: 4rem; 
     margin: 0;
     }
-    .info h1 p{
+    .info h1 p {
         position: relative;
         font: inherit;
         margin: 0;
@@ -59,23 +59,29 @@
         font-weight: 800;
         letter-spacing: -0.03em;
     }
-    .info h2{
+    .info h2 {
         max-width: 65ch;
         font-weight: 200;
         margin-top: 0.50rem;
     }
+    .background-div {
+        background-color: #ecebe8;
+        width: 100%;
+        height: 56vh;
+        margin: -205px auto;
+    }
 
-    .info .info-text{
+    .info .info-text {
         position: relative;
     }
-    .scroll-button{
+    .scroll-button {
         display: flex;
         border: none;
         outline: none;
         cursor: pointer;
     }
     
-    .scroll-button .arrow{
+    .scroll-button .arrow {
         margin-left: 0.50rem;
     }
     </style>
