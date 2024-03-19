e2a773adbfc4385815c5362b197ae89af6f7064b
Kristina <tinka200507@mail.ru>
2024-03-20 00:35:02 +0400

    add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,9 @@
     <div class="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
         <div class="nav-links">
-            <a href="/about" class="link">Обо мне</a>
-            <a href="/blog" class="link">Блог</a>
-            <a href="/projects" class="link">Портфолио</a>
-            <a href="/contact" class="link">Контакты</a>
+            {#each nav as link}
+            <a href={link.href} class="link">{link.title}</a>
+            {/each}
         </div>
     </div>
 </nav>
@@ -14,6 +13,30 @@
     <slot></slot>
 </div>
 
+<script>
+    const nav = [
+        {
+            title:'Обо мне',
+            href:'/about'
+        },
+        {
+            title:'Портфолио',
+            href:'/project'
+        },
+        {
+            title:'Контакты',
+            href:'/concact'
+        },
+        {
+            title:'temp',
+            href:'/temp'
+        },
+        {
+        title: "temp",
+        href: "/temp"
+        }
+    ]
+</script>
 <style>
     .container{
         max-width: 1400px;
